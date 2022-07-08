d65d5b3d07f307b462787d1fe71ef3b0201f3629
marcelo <bodao74@gmail.com>
2022-07-08 20:56:19 -0300

    Atualizado funcionando user e servers......

---
+++ app-routing/src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { UsersComponent } from './users/users.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { UserComponent } from './users/user/user.component';
+import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'users', component: UsersComponent, children: [
+      { path: ':id/:name', component: UserComponent }]
+  },
+  {
+    path: 'servers', component: ServersComponent, children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent }
+    ]
+  },
+  {path:'**', component:PageNotFoundComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ServersComponent,
+    UsersComponent,
+    EditServerComponent,
+    ServerComponent,
+    UserComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [ServersService, AuthService, AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app-routing/src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { queueScheduler } from 'rxjs';
+
+import { ServersService } from '../servers.service';
+
+@Component({
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css']
+})
+export class EditServerComponent implements OnInit {
+
+  server: { id: number, name: string, status: string };
+  serverName = '';
+  serverStatus = '';
+  allowEdit = false;
+  changesSaved = false;
+
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(
+      (queryParams: Params) => {
+        this.allowEdit = (+queryParams['allowEdit'] === 1 ? true : false);
+        console.log('Propriedade: ' + this.allowEdit);
+        console.log('Parametro === 1: '+ (queryParams['allowEdit'] === '1') );
+      }
+    );
+    this.route.fragment.subscribe();
+    this.server = this.serversService.getServer(+this.route.snapshot.params['id']);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+
+    console.log('Srv Status: ' + this.serverStatus);
+
+  }
+
+  onUpdateServer() {
+    this.serversService.updateServer(this.server.id, { name: this.server.name, status: this.serverStatus });
+    this.changesSaved = true;
+    this.router.navigate(['./'], { relativeTo: this.route });
+  }
+
+}
